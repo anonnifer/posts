4f63ea9ba4326be67398ef6248bc6356bf733da4
anonnifer <142012230+anonnifer@users.noreply.github.com>
2023-08-11 21:50:50 +1000

    Create write_better_python_code.py

---
+++ submissions/write_better_python_code.py
@@ -0,0 +1,28 @@
+---
+title: "write better python code"
+date: 2023-08-11T00:00:00+10:00
+draft: true
+keywords:
+  - data science
+  - machine learning
+  - predictive models
+  - python tests
+tags:
+  - data science
+  - machine learning
+  - python
+author: "@the.engineer"
+robots: index,follow
+---
+
+As your python code base gets larger, increases in complexity, and the number of contributors grow, it's crucial to uphold the quality, readability and currency of the code. Write better python code with the many resources available to help you with this.
+
+## black
+
+## pytest
+
+## pylint
+
+## mypy
+
+## yamllint
